5ee74b1448219555ca7ce2e3d74b0f5c5c97aca6
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-07-21 21:55:56 -0500

    comitting to test on live site

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Ouranxiety.org coming soon!<br></br><br></br>
+          -federico
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
