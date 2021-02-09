6e77080c3461837ccd2013b6b79938a0e99997a0
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2021-02-09 09:10:34 -0600

    It looks like the delete functinality is 100%

--- src/store/actions/deleteStory.js
+++ src/store/actions/deleteStory.js
@@ -24,19 +24,29 @@ export const deleteStoryFail = (error) => {
     };
 };
 
+export const fetchStoriesSuccess = (stories) => {
+    return {
+        type: actionTypes.FETCH_STORIES_SUCCESS,
+        stories: stories
+    };
+};
+
 export const deleteStory = (id, data) => {
     return dispatch => {
         dispatch(deleteStoryStart());
         axios.delete('http://localhost:3000/stories/' + id, data)
         // .then(res => console.log('where is my data', res.status))
         .then(res => {
-            dispatch(deleteStorySuccess(res.data));
-            window.onload = function() {
-                if(!window.location.hash) {
-                    window.location = window.location + '#loaded';
-                    window.location.reload();
+            dispatch(deleteStorySuccess(res.data))
+        // I need to get all posts in order for my page to update the state without the deleted posts
+            axios.get('http://localhost:3000/stories')
+            .then(res => {
+                // console.log('first test', res.data)
+             
+                
+                dispatch(fetchStoriesSuccess(res.data))
                 }
-            };
+            )
         })
         .catch(error => { 
             dispatch(deleteStoryFail(error))
